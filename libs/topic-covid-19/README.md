11de8056e5f2c9ec155c8918745f410516d50066
addshore <adamshorland@gmail.com>
2020-03-20 18:01:14 +0000

    wm:wd:covid19-value command start
    This allows easy importing of WHO data from PDFs into wikidata
    for death and case rates of covid-19.
    
    Messy, but working...

---
+++ libs/topic-covid-19/README.md
@@ -0,0 +1,1 @@
+# Topic covid-19
--- libs/wikimedia-commands/addCommands.php
+++ libs/wikimedia-commands/addCommands.php
@@ -5,5 +5,6 @@ $GLOBALS['awwCommands'][] = function ( $awwConfig ) {
 		new \Addwiki\Commands\Wikimedia\ExtensionToWikidata( $awwConfig ),
 		new \Addwiki\Commands\Wikimedia\WikidataReferenceDateFixer( $awwConfig ),
 		new \Addwiki\Commands\Wikimedia\WikidataReferencer\WikidataReferencerCommand( $awwConfig ),
+		new \Addwiki\Commands\Wikimedia\WikidataCovid19\ImportWHOReportValueCommand( $awwConfig ),
 	);
 };
