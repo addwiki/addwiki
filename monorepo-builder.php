5cdf02a14fb2354b17ecb5e826619a82529890c2
addshore <addshorewiki@gmail.com>
2021-01-24 10:52:00 +0000

    Switch to symplify/monorepo-builder

---
+++ monorepo-builder.php
@@ -0,0 +1,24 @@
+<?php
+
+declare(strict_types=1);
+
+use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
+use Symplify\ComposerJsonManipulator\ValueObject\ComposerJsonSection;
+use Symplify\MonorepoBuilder\ValueObject\Option;
+
+return static function (ContainerConfigurator $containerConfigurator): void {
+    $parameters = $containerConfigurator->parameters();
+
+    // where are the packages located?
+    $parameters->set(Option::PACKAGE_DIRECTORIES, [
+        __DIR__ . '/packages',
+        __DIR__ . '/packages-dev',
+    ]);
+
+    // for "merge" command
+    $parameters->set(Option::DATA_TO_APPEND, [
+        ComposerJsonSection::REQUIRE_DEV => [
+            'phpunit/phpunit' => '~4.8.0 || ~5.3.0',
+        ],
+    ]);
+};
