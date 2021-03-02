8e2c89d39f0f71b65f223fe77fe730845b630431
addshore <addshorewiki@gmail.com>
2021-03-02 23:12:19 +0000

    Don't use ApiUser in UserIntegrationTest.php

--- packages/mediawiki-api/tests/integration/Service/UserIntegrationTest.php
+++ packages/mediawiki-api/tests/integration/Service/UserIntegrationTest.php
@@ -2,26 +2,22 @@
 
 namespace Addwiki\Mediawiki\Api\Tests\Integration\Service;
 
-use Addwiki\Mediawiki\Api\Client\ApiUser;
 use Addwiki\Mediawiki\Api\Tests\Integration\TestEnvironment;
 use PHPUnit\Framework\TestCase;
 
 class UserIntegrationTest extends TestCase {
 
-	private static ?ApiUser $localApiUser = null;
-
 	public static function setUpBeforeClass(): void {
 		parent::setUpBeforeClass();
-		$strTime = strval( time() );
-		self::$localApiUser = new ApiUser( 'TestUser - ' . strval( time() ), $strTime . '-pass' );
 	}
 
 	public function testCreateUser(): void {
+		$strTime = strval( time() );
+
 		$factory = TestEnvironment::newInstance()->getFactory();
 		$createResult = $factory->newUserCreator()->create(
-			self::$localApiUser->getUsername(),
-
-			self::$localApiUser->getPassword()
+			'TestUser - ' . $strTime,
+			$strTime . '-pass'
 		);
 		$this->assertTrue( $createResult );
 	}
