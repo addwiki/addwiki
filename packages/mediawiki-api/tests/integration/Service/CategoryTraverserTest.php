a5bb1c40fa6e062a67e5288bbb63a6a1f84b8885
addshore <addshore@users.noreply.github.com>
2021-04-10 09:51:18 +0100

    Update CategoryTraverserTest.php

--- packages/mediawiki-api/tests/integration/Service/CategoryTraverserTest.php
+++ packages/mediawiki-api/tests/integration/Service/CategoryTraverserTest.php
@@ -29,13 +29,12 @@ class CategoryTraverserTest extends TestCase {
 	}
 
 	/**
-	 * A convenience wrapper around a PageDeleter.
-	 * @param string[] $titles The titles to delete.
+	 * A convenience wrapper to blank pages.
+	 * TODO actually delete them?
+	 * @param string[] $titles The titles to blank.
 	 */
-	public function deletePages( array $titles ): void {
+	public function blankPages( array $titles ): void {
 		foreach ( $titles as $t ) {
-			// @todo Properly delete?
-			// $deleter->deleteFromPageTitle( new Title( $t ) );
 			$this->savePage( $t, '' );
 		}
 	}
@@ -79,7 +78,7 @@ class CategoryTraverserTest extends TestCase {
 		$this->traverser->addCallback( CategoryTraverser::CALLBACK_PAGE, $callback );
 		$decendants = $this->traverser->descend( $rootCat );
 		$this->assertCount( 4, $decendants->toArray() );
-		$this->deletePages( [
+		$this->blankPages( [
 			'Category:Root category',
 			'Category:Sub category B',
 			'Category:Sub category C',
@@ -105,7 +104,7 @@ class CategoryTraverserTest extends TestCase {
 		$this->testEnvironment->runJobs();
 		$decendants = $this->traverser->descend( $grandparent );
 		$this->assertCount( 4, $decendants->toArray() );
-		$this->deletePages( [
+		$this->blankPages( [
 			'Category:Grandparent',
 			'Category:Parent 1',
 			'Category:Parent 2',
@@ -144,7 +143,7 @@ class CategoryTraverserTest extends TestCase {
 		$descendants = $this->traverser->descend( $catA );
 		$this->assertCount( 0, $descendants->toArray() );
 		$this->assertCount( 3, $wgVisitedCats );
-		$this->deletePages( [
+		$this->blankPages( [
 			'Category:A cat',
 			'Category:B cat',
 			'Category:C cat',
@@ -186,7 +185,7 @@ class CategoryTraverserTest extends TestCase {
 			$this->assertEquals( $expectedCatLoop, $actualCatLoop );
 		}
 		$this->assertTrue( $haveCaught );
-		$this->deletePages( [
+		$this->blankPages( [
 			'Category:E cat',
 			'Category:F cat',
 			'Category:G cat',
